2bc9ec87bae1853605db670d57b08f00b244cca0
xulihang <xulihanghai@163.com>
2022-09-06 15:53:13 +0800

    update import of gcdwebserver

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { GCDWebServer } from 'awesome-cordova-plugin-gcdwebserver';
+import { GCDWebServer } from '@awesome-cordova-plugins/gcdwebserver';
 
 @Component({
   selector: 'app-home',
